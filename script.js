a47228b6abf9c8d3eb67adb8cc8df93a62b756c6
omnibus1 <appleofeden8686@gmail.com>
2024-09-26 21:26:40 +0200

    day 2 :(

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-console.log(window.innerWidth)
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.fromTo("#small-yellow", {x: "100vw"}, {x: "0px", duration: 2, repeat: -1, yoyo: true})
 
-gsap.fromTo("#medium-red", {x: "100vw"}, {x: "10vw", duration: 3, repeat: -1, yoyo: true})
 
-gsap.fromTo("#medium-yellow", {x:"0vw"}, {x:"100vw", duration: 2, repeat: -1, yoyo: true})
+gsap.timeline().fromTo(".initial-fade-in", {opacity: 0}, {opacity: 1, duration: 0.25, stagger: 0.35})
 
-gsap.timeline().fromTo(".initial-fade-in", {opacity: 0}, {opacity: 1, duration: 0.5, stagger: 0.5})
+gsap.timeline()
+    .from("#blue-box", {scale: 150,  duration: 2, ease: "power2.out"})
+    .fromTo(".name", {opacity: 0, y: 100}, {opacity: 1, y: 0, stagger: 0.01, duration: 0.5})
 
-// gsap.from("#blue-box", {height: "100vh", width: "100vh", duration: 1, delay: 1})
+gsap.fromTo("#blue-box", {scale: 1}, {scale: 0.5, backgroundColor: '#ff87ef',  scrollTrigger: {trigger: "#blue-box", start: "top 30%", scrub: true}});
+
+
+
+
+
+// gsap.from("#agh-logo", {scale: 0.00, scrollTrigger: "#agh-logo"})
+
+// gsap.fromTo("#agh-logo", {scale: 1}, {scale: 0.7, scrollTrigger: {trigger: "#agh-logo", start: "top 5%", scrub: true}});
+
+gsap.to("#agh-logo", {y:10, duration:3, repeat: -1, yoyo: true, ease: "back.inOut"})
+
+gsap.fromTo(".show-text", {opacity: 0}, {opacity: 1, stagger: 0.5, duration: 0.5, scrollTrigger: {trigger: ".show-text", start: "top 80%"}})
+
+gsap.fromTo("#agh-logo", {rotation: 15}, {rotation: -15, duration: 5, repeat: -1, yoyo: true})
+
