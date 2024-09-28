0a689f3e6e77c60aafead2870f44a97ab757f8dc
omnibus1 <appleofeden8686@gmail.com>
2024-09-29 00:30:21 +0200

    day 4

--- script.js
+++ script.js
@@ -12,12 +12,16 @@ gsap.timeline()
     .fromTo(".name", {opacity: 0, y: 100}, {opacity: 1, y: 0, stagger: 0.01, duration: 0.5})
 
 //name scrolltrigger
-gsap.fromTo("#blue-box", {scale: 1}, {scale: 0.8, backgroundColor: '#000000', color: "black", scrollTrigger: {trigger: "#blue-box", start: "bottom 30%", end: "bottom 0%", scrub: true}});
+gsap.fromTo("#blue-box", {scale: 1}, {scale: 0.8, backgroundColor: '#FFFFFF', color: "black", scrollTrigger: {trigger: "#blue-box", start: "bottom 15%", end: "bottom 5%", scrub: true}});
 
 //about text scrolltrigger
 gsap.fromTo(".about-text-show", {opacity: 0}, {opacity: 1, delay: 0.5, stagger: 0.5, duration: 0.5, scrollTrigger: {trigger: ".show-text", start: "top 80%"}})
 gsap.fromTo(".about-skills-show", {opacity: 0}, {opacity: 1, delay: 0.5, stagger: 0.5, duration: 0.5, scrollTrigger: {trigger: ".about-skills-show", start: "top 80%"}})
 
+//not used but this can be usefull in the future
+//about color change
+// gsap.to("#change-bg", { backgroundColor: "#80ED99", color: "white",
+//     scrollTrigger:{trigger: "#expierience", start: "bottom center", end: "600% center", scrub: true, toggleActions:"play none reverse none"}})
 
 //agh logo
 gsap.from("#agh-logo",
@@ -32,5 +36,7 @@ document.querySelector("#agh-logo").addEventListener("mouseleave", () => {
 gsap.to("#agh-logo", { scale: 1, duration: 0.5, ease: "power2.out" });
 });
 
-// gsap.to("#agh-logo", {x:0, yoyo: true, ease: "back.inOut"})
+//expierience scroll trigger
+
+gsap.from(".expierience-show", {opacity: 0, stagger: 0.5, duration: 0.5, scrollTrigger:{trigger: ".expierience-show", start: "bottom 85%"}})
 
